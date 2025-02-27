bb288d36ab73318d6bda4b4dc51abed3227a16ae
jinhyeon <tiemo0708@naver.com>
2025-02-27 17:38:18 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/ClubAccount_BE/ClubAccountBeApplication.java
@@ -0,0 +1,13 @@
+package com.ClubAccount_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubAccountBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubAccountBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ClubAccount_BE/ClubAccountBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClubAccount_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubAccountBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
