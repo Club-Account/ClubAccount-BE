3d6ee12079ebace51e6fe2ec5ea721a501ecefeb
baekjaehyuk <bjh010321@naver.com>
2025-03-05 18:24:54 +0900

    TB-2/chore: Spring Security 비활성화

--- src/main/java/com/ClubAccount_BE/ClubAccountBeApplication.java
+++ src/main/java/com/ClubAccount_BE/ClubAccountBeApplication.java
@@ -2,12 +2,13 @@ package com.ClubAccount_BE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ClubAccountBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClubAccountBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClubAccountBeApplication.class, args);
+    }
 
 }
