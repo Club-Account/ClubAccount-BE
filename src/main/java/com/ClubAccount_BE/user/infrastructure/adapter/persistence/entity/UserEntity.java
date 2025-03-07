1054f5bd67a8815f6b7b080a7b073453395a3410
jinhyeon <tiemo0708@naver.com>
2025-03-07 17:26:44 +0900

    TB-3/feat: 유저 엔티티 생성

---
+++ src/main/java/com/ClubAccount_BE/user/infrastructure/adapter/persistence/entity/UserEntity.java
@@ -0,0 +1,40 @@
+package com.ClubAccount_BE.user.infrastructure.adapter.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+
+@Table(name = "user")
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String department;
+
+    @Column(nullable = false)
+    private String role;
+    
+    private String profileUrl;
+
+    private String rink;
+}
