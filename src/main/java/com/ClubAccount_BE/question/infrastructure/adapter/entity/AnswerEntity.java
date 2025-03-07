28e0b642eb181bac3c0a8cf0e28a2cd0469d3f06
jinhyeon <tiemo0708@naver.com>
2025-03-07 17:27:20 +0900

    TB-3/feat: 질문&답변 엔티티 생성

---
+++ src/main/java/com/ClubAccount_BE/question/infrastructure/adapter/entity/AnswerEntity.java
@@ -0,0 +1,38 @@
+package com.ClubAccount_BE.question.infrastructure.adapter.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Table(name = "answer")
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AnswerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false, unique = true)
+    private QuestionEntity question;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime updateAt;
+}
